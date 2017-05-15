4f8826a4244d425cfb9a5aceaf345fa3241ff751
ariovistus <ellery-newcomer@utulsa.edu>
2017-05-15 16:18:54 -0700

    add repo, other fixups
    to package.json

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function configure(aurelia, config) {
+}
+exports.configure = configure;
+//# sourceMappingURL=index.js.map
