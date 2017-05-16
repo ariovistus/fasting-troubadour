2be465d1e24f31f4f432a2889e9cb5d8ad22733c
ariovistus <ellery-newcomer@utulsa.edu>
2017-05-16 09:52:55 -0700

    statically list resources?

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+require("./alphanumeric-input-behavior");
+require("./formatted-number-behavior");
+require("./name-input-behavior");
+require("./number-format");
+require("./numeric-input-behavior");
+require("./numeric-name-input-behavior");
+require("./trim-binding-behavior");
 function configure(aurelia, config) {
 }
 exports.configure = configure;
--- dist/index.ts
+++ dist/index.ts
@@ -1,3 +1,11 @@
+import "./alphanumeric-input-behavior";
+import "./formatted-number-behavior";
+import "./name-input-behavior";
+import "./number-format";
+import "./numeric-input-behavior";
+import "./numeric-name-input-behavior";
+import "./trim-binding-behavior";
+
 export function configure(aurelia, config) {
 }
 
