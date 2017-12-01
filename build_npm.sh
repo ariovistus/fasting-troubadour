e43d28a3f0632b6da890c79d665b90d1e4e335b3
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-12-01 07:39:20 -0800

    docker y u no work

--- build_npm.sh
+++ build_npm.sh
@@ -1,5 +1,6 @@
 rm -rf dist/*
 mkdir -p dist 
+docker --version
 docker run -v $(pwd):/app --entrypoint yarn sandrokeil/typescript install
 docker run -v $(pwd):/app --entrypoint python sandrokeil/typescript versionchecker.py
 docker run -v $(pwd):/app --entrypoint ./node_modules/.bin/typings sandrokeil/typescript install
